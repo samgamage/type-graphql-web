dcd7727fba39ec315548056b35b29a48a64098fb
sgamage2018 <sgamage2018@my.fit.edu>
2019-01-29 09:14:01 -0500

    changes

--- src/routes/profile-pic.js
+++ src/routes/profile-pic.js
@@ -1,32 +1,34 @@
 import gql from "graphql-tag";
 import React from "react";
 import { Mutation } from "react-apollo";
+import Dropzone from "react-dropzone";
 
 const addProfilePictureMutation = gql`
-  mutation AddProfilePicture($picture: Upload!) {
-    addProfilePicture(picture: $picture)
+  mutation AddProfilePicture($file: Upload!) {
+    addProfilePicture(file: $file)
   }
 `;
 
 const ProfilePic = () => (
-  // <Query query={meQuery}>
-  //   {({ data: { me } }) => (
   <Mutation mutation={addProfilePictureMutation}>
     {(addProfilePicture) => (
-      <input
-        type="file"
-        required
-        onChange={({
-          target: {
-            validity,
-            files: [file],
-          },
-        }) => validity.valid && addProfilePicture({ variables: { picture: file } })}
-      />
+      <Dropzone
+        onDrop={([file]) => {
+          console.log(file);
+          addProfilePicture({ variables: { file } });
+        }}
+        accept="image/jpeg, image/png"
+        multiple={false}
+      >
+        {({ getRootProps, getInputProps }) => (
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p>Try dropping some files here, or click to select files to upload.</p>
+          </div>
+        )}
+      </Dropzone>
     )}
   </Mutation>
-  //   )}
-  // </Query>
 );
 
 export default ProfilePic;
